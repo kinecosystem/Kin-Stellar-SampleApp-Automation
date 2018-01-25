db19c5f65283613dcb6aa8809a4f2542ecac2910
Ronserruya <ron.serruya@kik.com>
2018-01-25 16:08:36 +0200

    Class methods for Setup/teardown

--- iOS_Sanity.py
+++ iOS_Sanity.py
@@ -1,13 +1,15 @@
 from appium import webdriver
 import unittest
+import time
 
 class TestCases(unittest.TestCase):
 
     # Set up Appium and the app
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         # Sample App should be already installed on the emulator # TODO: add option to install
         bundleId = 'com.kinfoundation.KinSampleApp'
-        self.driver = webdriver.Remote(
+        cls.driver = webdriver.Remote(
             command_executor = 'http://127.0.0.1:4723/wd/hub', # Run on local server
             desired_capabilities = {
                 'bundleId': bundleId,
@@ -16,24 +18,25 @@ class TestCases(unittest.TestCase):
                 'deviceName': 'iPhone 7'  # TODO: name and version from somewhere else
             }
         )
-        self._values = []
+        cls._values = []
 
     # Gets the test run is over
-    def tearDown(self):
-        self.driver.quit()
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
 
     # List of tests from <link to test cases>
-    def test_mainScreen(self):
+    def test_1_mainScreen(self):
         testNetButton = self.driver.find_element_by_accessibility_id('TestNetButton')
         mainNetButton = self.driver.find_element_by_accessibility_id('MainNetButton')
 
         self.assertIsNotNone(testNetButton)
         self.assertIsNotNone(mainNetButton)
 
-    def test_failthis(self):
+    def test_2_failthis(self):
         # This should fail
         testNetButton = self.driver.find_element_by_accessibility_id('TestNetButton')
-        self.assertIsNone(testNetButton)
+        self.assertIsNone(None)
 
 
 
